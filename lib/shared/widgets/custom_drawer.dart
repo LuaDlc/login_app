3cec358a12d0d83dd0351960cc650057d4338d59
Luana Dias <luana.dias@colab.re>
2023-10-21 19:50:52 -0300

    showAlertDialog

--- lib/shared/widgets/custom_drawer.dart
+++ lib/shared/widgets/custom_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_app/pages/login_page.dart';
 
 import '../../pages/dados_cadastrais.dart';
 
@@ -13,16 +14,50 @@ class CustomDrawer extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           //componente que cria tipo um containter com os dados inseridos ans propriedades
-          UserAccountsDrawerHeader(
-              decoration: BoxDecoration(color: Colors.deepPurple[400]),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: Colors.deepPurple,
-                child: Image.asset('assets/avatar.png'),
-              ),
-              accountName: const Text("Luana Dias"),
-              accountEmail: const Text("Email@email.com")),
+          GestureDetector(
+            onTap: () {
+              showModalBottomSheet(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Wrap(children: [
+                      Column(
+                        children: [
+                          ListTile(
+                            contentPadding: const EdgeInsets.all(8.0),
+                            title: const Text('camera'),
+                            leading: IconButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                icon: const Icon(Icons.camera_alt_outlined)),
+                          ),
+                          ListTile(
+                            contentPadding: const EdgeInsets.all(8.0),
+                            title: const Text('album'),
+                            leading: IconButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                icon: const Icon(Icons.album_rounded)),
+                          ),
+                        ],
+                      )
+                    ]);
+                  });
+            },
+            child: UserAccountsDrawerHeader(
+                decoration: BoxDecoration(color: Colors.deepPurple[400]),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.deepPurple,
+                  child: Image.asset('assets/avatar.png'),
+                ),
+                accountName: const Text("Luana Dias"),
+                accountEmail: const Text("Email@email.com")),
+          ),
           const SizedBox(
-            height: 20,
+            height: 15,
           ),
           GestureDetector(
             onTap: () {
@@ -51,11 +86,11 @@ class CustomDrawer extends StatelessWidget {
             ),
           ),
           const SizedBox(
-            height: 20,
+            height: 15,
           ),
           const Divider(),
           const SizedBox(
-            height: 20,
+            height: 15,
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
@@ -67,19 +102,46 @@ class CustomDrawer extends StatelessWidget {
                 ),
                 GestureDetector(
                   onTap: () {
-                    print('sasa');
+                    showModalBottomSheet(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 12, vertical: 16),
+                            child: const SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'Termos de uso e privacidade',
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    'Ao contrário do que se acredita, Lorem Ipsum não é simplesmente um texto randômico. Com mais de 2000 anos, suas raízes podem ser encontradas em uma obra de literatura latina clássica datada de 45 AC. Richard McClintock, um professor de latim do Hampden-Sydney College na Virginia, pesquisou uma das mais obscuras palavras em latim, consectetur, oriunda de uma passagem de Lorem Ipsum, e, procurando por entre citações da palavra na literatura clássica, descobriu a sua indubitável origem. Lorem Ipsum vem das seções 1.10.32 e 1.10.33 do "de Finibus Bonorum et Malorum" (Os Extremos do Bem e do Mal), de Cícero, escrito em 45 AC. Este livro é um tratado de teoria da ética muito popular na época da Renascença. A primeira linha de Lorem Ipsum, "Lorem Ipsum dolor sit amet..." vem de uma linha na seção 1.10.32',
+                                    style: TextStyle(fontSize: 12),
+                                    textAlign: TextAlign.justify,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        });
                   },
-                  child: const Text('Termos de uso e privacidade'),
+                  child: const Text(
+                    'Termos de uso e privacidade',
+                    style: TextStyle(fontSize: 16),
+                  ),
                 ),
               ],
             ),
           ),
           const SizedBox(
-            height: 20,
+            height: 15,
           ),
           const Divider(),
           const SizedBox(
-            height: 20,
+            height: 15,
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
@@ -90,17 +152,64 @@ class CustomDrawer extends StatelessWidget {
                   width: 5,
                 ),
                 GestureDetector(
-                    onTap: () {
-                      print('sasa');
-                    },
-                    child: const Text('configuracoes')),
+                    onTap: () {}, child: const Text('configuracoes')),
               ],
             ),
           ),
           const SizedBox(
-            height: 20,
+            height: 15,
           ),
           const Divider(),
+          const SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: Row(
+              children: [
+                const Icon(Icons.exit_to_app_rounded),
+                const SizedBox(
+                  width: 5,
+                ),
+                GestureDetector(
+                    onTap: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              alignment: Alignment.centerLeft,
+                              elevation: 8,
+                              title: const Wrap(children: [Text('Sair')]),
+                              content: const Text('Deseja sair do app?'),
+                              actions: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    const LoginPage())); //usuario retorna á tela de login
+                                      },
+                                      child: const Text('Sair'),
+                                    ),
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: const Text('voltar'))
+                                  ],
+                                )
+                              ],
+                            );
+                          });
+                    },
+                    child: const Text('Sair')),
+              ],
+            ),
+          ),
         ],
       ),
     );
