239186a1bbdf515cf97de5f0737d65cf6f0854cd
Luana Dias <luana.dias@colab.re>
2023-10-22 15:58:54 -0300

    linearProgressIndicator e simulacao de api

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:login_app/model/card_detail.dart';
 import 'package:login_app/pages/card_detail.dart';
+import 'package:login_app/repository/card_detail_repository.dart';
 
 class CardPage extends StatefulWidget {
   const CardPage({super.key});
@@ -10,65 +11,79 @@ class CardPage extends StatefulWidget {
 }
 
 class _CardPageState extends State<CardPage> {
-  var cardDetail = CardDetail(
-      id: 1,
-      title: "meu card",
-      url: "https://hermes.digitalinnovation.one/assets/diome/logo.png",
-      text:
-          'Ao contrário do que se acredita, Lorem Ipsum não é simplesmente um texto randômico. Com mais de 2000 anos, suas raízes podem ser encontradas em uma obra de literatura latina clássica datada de 45 AC. Richard McClintock, um professor de latim do Hampden-Sydney College na Virginia, pesquisou uma das mais obscuras palavras em latim, consectetur, oriunda de uma passagem de Lorem Ipsum, e, procurando por entre citações da palavra na literatura clássica, descobriu a sua indubitável origem. Lorem Ipsum vem das seções 1.10.32 e 1.10.33 do "de Finibus Bonorum et Malorum" (Os Extremos do Bem e do Mal), de Cícero, escrito em 45 AC. Este livro é um tratado de teoria da ética muito popular na época da Renascença. A primeira linha de Lorem Ipsum, "Lorem Ipsum dolor sit amet..." vem de uma linha na seção 1.10.32.');
+  CardDetail? cardDetail; //precisa de um repository inicializado e um initState
+  var cardDetailRepository = CardDetailRepository();
+
+  @override
+  void initState() {
+    super.initState();
+    carregarDados(); //precisa chamar a funcao aqui pra iniciar o carregamento de dados
+  }
+
+  void carregarDados() async {
+    cardDetail =
+        await cardDetailRepository.get(); //a funcao pode ser future, com async
+    //initsatet nao pode ser, por isso a funcao foi criada. aguardar o carregamento dos dados
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Container(
           margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          child: GestureDetector(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                          CardDetailPage(cardDetail: cardDetail)));
-            },
-            child: Hero(
-              tag: cardDetail.id ?? '',
-              child: Card(
-                elevation: 8,
-                shadowColor: Colors.grey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 8),
-                      child: Row(
+          child: cardDetail == null
+              ? const LinearProgressIndicator()
+              : GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => CardDetailPage(
+                                cardDetail:
+                                    cardDetail!))); //usamos o ! pois nao pode ser nulo
+                  },
+                  child: Hero(
+                    tag: cardDetail!.id ?? '',
+                    child: Card(
+                      elevation: 8,
+                      shadowColor: Colors.grey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Image.network(
-                            cardDetail.url ?? '',
-                            height: 20,
-                            width: 20,
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 8),
+                            child: Row(
+                              children: [
+                                Image.network(
+                                  cardDetail!.url ?? '',
+                                  height: 20,
+                                  width: 20,
+                                ),
+                                Text(
+                                  cardDetail!.title!,
+                                  style: const TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
                           ),
-                          Text(
-                            cardDetail.title!,
-                            style: const TextStyle(
-                                fontSize: 17, fontWeight: FontWeight.bold),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 12, vertical: 12),
+                            child: Text(
+                              cardDetail!.text ?? '',
+                              textAlign: TextAlign.justify,
+                            ),
                           ),
                         ],
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 12, vertical: 12),
-                      child: Text(
-                        cardDetail.text ?? '',
-                        textAlign: TextAlign.justify,
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          ),
         )
       ],
     );
